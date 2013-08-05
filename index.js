87d89d71a1c6bba8242cc4b49d0b3c3c34f108f7
you21979 <you2197901@gmail.com>
2013-08-05 17:30:39 +0900

    rename

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 var heartbeats = require("./lib/heartbeats");
-var MysqlCluster = require("./lib/mysql_cluster");
+var MysqlHorizontalPartition = require("./lib/mysql_horizontal_partition");
+var MysqlVerticalPartition = require("./lib/mysql_vertical_partition");
 exports.initialize = function(timer){
     heartbeats.start(timer || 120 * 1000);
 }
 exports.finalize = function(){
     heartbeats.end();
 }
-exports.createCluster = function(clustername){
-    return new MysqlCluster(clustername);
+exports.createHorizontalPartition = function(name){
+    return new MysqlHorizontalPartition(name);
+}
+exports.createVerticalPartition = function(name){
+    return new MysqlVerticalPartition(name);
 }
