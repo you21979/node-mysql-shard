d0fea3ce86200866fcdfe0a3bd291013a9f8acf5
you21979 <you2197901@gmail.com>
2013-08-05 18:00:11 +0900

    mod

--- test/test.js
+++ test/test.js
@@ -11,29 +11,48 @@ PoolUtil.event.onConnect = function(conn, latency){
 
 var fs = require('fs');
 var cfgAuth = JSON.parse(fs.readFileSync("../conf/mysql_auth.json"));
-var cfgShard = JSON.parse(fs.readFileSync("../conf/mysql_horizontal.json"));
+var cfgHorizontal = JSON.parse(fs.readFileSync("../conf/mysql_horizontal.json"));
+var cfgVertical = JSON.parse(fs.readFileSync("../conf/mysql_vertical.json"));
 
-var cluster = MysqlShard.createHorizontalPartition("hoge");
+var horizontal = MysqlShard.createHorizontalPartition("hoge");
+var vertical = MysqlShard.createVerticalPartition("hoge");
 
 var pool = 5;
-cluster.connect(cfgAuth.auth.user, cfgAuth.auth.password, pool, cfgShard, function(){
-    console.log("ok");
-    var db = cluster.select(1);
+horizontal.connect(cfgAuth.auth.user, cfgAuth.auth.password, pool, cfgHorizontal, function(){
+    console.log("hok");
+    var db = horizontal.select(1);
     var timeout = 1 * 1000;
     for(var i=0;i<100;++i){
         db.getMaster(timeout, function(err, conn){
             conn.query("select 1;", function(err, res){
                 console.log(res);
-                conn.end();
+                conn.quit();
             });
         });
     }
 });
-cluster.on("error", function(param){
+horizontal.on("error", function(param){
+    console.log(param.reason);
+})
+vertical.connect(cfgAuth.auth.user, cfgAuth.auth.password, pool, cfgVertical, function(){
+    console.log("vok");
+    var db = vertical.select("gen_player_seqid");
+    var timeout = 1 * 1000;
+    for(var i=0;i<100;++i){
+        db.getMaster(timeout, function(err, conn){
+            conn.query("select 2;", function(err, res){
+                console.log(res);
+                conn.quit();
+            });
+        });
+    }
+});
+vertical.on("error", function(param){
     console.log(param.reason);
 })
 setTimeout(function(){
-    cluster.close();
+    horizontal.close();
+    vertical.close();
     MysqlShard.finalize();
 }, 2 * 1000);
 
