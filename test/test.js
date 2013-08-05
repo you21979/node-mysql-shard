d158962ded2644536c0a3ef93edaea5b47cadb60
you21979 <you2197901@gmail.com>
2013-08-05 16:11:39 +0900

    mod

--- test/test.js
+++ test/test.js
@@ -1,6 +1,13 @@
 "use strict";
 var MysqlShard = require('../');
 MysqlShard.initialize(2000);
+var PoolUtil = require('../lib/pool_utils.js');
+PoolUtil.event.onPingResponse = function(conn, latency){
+    console.log(latency);
+}
+PoolUtil.event.onConnect = function(conn, latency){
+    console.log(latency);
+}
 
 var fs = require('fs');
 var cfg = JSON.parse(fs.readFileSync("../conf/mysql.json"));
@@ -11,8 +18,21 @@ var pool = 5;
 cluster.connect("root", "", pool, cfg, function(){
     console.log("ok");
     var db = cluster.select(1);
-    var timeout = 10000;
-    db.getMaster(timeout, function(err, conn){
-        conn.query("select * from mysql;");
-    });
+    var timeout = 1 * 1000;
+    for(var i=0;i<100;++i){
+        db.getMaster(timeout, function(err, conn){
+            conn.query("select 1;", function(err, res){
+                console.log(res);
+                conn.end();
+            });
+        });
+    }
 });
+cluster.on("error", function(param){
+    console.log(param.reason);
+})
+setTimeout(function(){
+    cluster.close();
+    MysqlShard.finalize();
+}, 2 * 1000);
+
